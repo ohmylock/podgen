e1c4517a310f2354b6d37e7168b918e2e808d23a
id <id@localhost.local>
2022-07-12 22:18:05 +0200

    Added feed generator

--- internal/app/podgen/proc/s3.go
+++ internal/app/podgen/proc/s3.go
@@ -14,26 +14,6 @@ type S3Store struct {
 	Bucket   string
 }
 
-// UploadEpisode to s3 storage
-func (s *S3Store) UploadEpisode(ctx context.Context, objectName, filePath string) (*minio.UploadInfo, error) {
-	exists, errBucketExists := s.Client.BucketExists(ctx, s.Bucket)
-	if errBucketExists != nil {
-		log.Fatalf("[ERROR] can't check exists bucket %s, %v", s.Bucket, errBucketExists)
-	}
-
-	if !exists {
-		err := s.Client.MakeBucket(ctx, s.Bucket, minio.MakeBucketOptions{Region: s.Location})
-		if err != nil {
-			log.Fatalf("[ERROR] can't create bucket %s, %v", s.Bucket, errBucketExists)
-		}
-	}
-	uploadInfo, err := s.Client.FPutObject(ctx, s.Bucket, objectName, filePath, minio.PutObjectOptions{ContentType: "audio/mp3"})
-	if err != nil {
-		return nil, err
-	}
-	return &uploadInfo, nil
-}
-
 // DeleteEpisode from s3 storage
 func (s *S3Store) DeleteEpisode(ctx context.Context, objectName string) error {
 	exists, errBucketExists := s.Client.BucketExists(ctx, s.Bucket)
@@ -48,4 +28,33 @@ func (s *S3Store) DeleteEpisode(ctx context.Context, objectName string) error {
 		return err
 	}
 	return nil
+}
+
+// UploadEpisode to s3 storage
+func (s *S3Store) UploadEpisode(ctx context.Context, objectName, filePath string) (*minio.UploadInfo, error) {
+	return s.uploadFile(ctx, objectName, filePath, "audio/mp3")
+}
+
+// UploadFeed to s3 storage
+func (s *S3Store) UploadFeed(ctx context.Context, objectName, filePath string) (*minio.UploadInfo, error) {
+	return s.uploadFile(ctx, objectName, filePath, "plain/text")
+}
+
+func (s *S3Store) uploadFile(ctx context.Context, objectName, filePath, contentType string) (*minio.UploadInfo, error) {
+	exists, errBucketExists := s.Client.BucketExists(ctx, s.Bucket)
+	if errBucketExists != nil {
+		log.Fatalf("[ERROR] can't check exists bucket %s, %v", s.Bucket, errBucketExists)
+	}
+
+	if !exists {
+		err := s.Client.MakeBucket(ctx, s.Bucket, minio.MakeBucketOptions{Region: s.Location})
+		if err != nil {
+			log.Fatalf("[ERROR] can't create bucket %s, %v", s.Bucket, errBucketExists)
+		}
+	}
+	uploadInfo, err := s.Client.FPutObject(ctx, s.Bucket, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		return nil, err
+	}
+	return &uploadInfo, nil
 }
