fc9d95725a09d8b5d5c3debe9fb9c1388b6d0096
id <id@localhost.local>
2022-07-15 21:34:56 +0200

    Removed not released tests

--- internal/app/podgen/app_test.go
+++ internal/app/podgen/app_test.go
@@ -9,22 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestApp_DeleteOldEpisodes(t *testing.T) {
-
-}
-
-func TestApp_GenerateFeed(t *testing.T) {
-
-}
-
-func TestApp_Update(t *testing.T) {
-
-}
-
-func TestApp_UploadEpisodes(t *testing.T) {
-
-}
-
 func TestNewBoltDB(t *testing.T) {
 	tmpFile, _ := ioutil.TempFile("", "")
 	defer func(name string) {
@@ -37,12 +21,4 @@ func TestNewBoltDB(t *testing.T) {
 	db, err := NewBoltDB(tmpFile.Name())
 	assert.NoError(t, err)
 	assert.NotNil(t, db)
-}
-
-func TestApp_FindPodcasts(t *testing.T) {
-
-}
-
-func TestNewS3Client(t *testing.T) {
-
 }
